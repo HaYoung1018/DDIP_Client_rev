ba6b30821728184f31d98782326ab1eefc1cb970
hae <godma531@naver.com>
2024-12-06 20:16:13 +0900

    인터넷

--- app/src/main/java/com/example/ddip_client/network/RetrofitClient.java
+++ app/src/main/java/com/example/ddip_client/network/RetrofitClient.java
@@ -9,9 +9,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://172.30.1.41:8080/";
+    private static final String BASE_URL = "http://172.30.1.39:8080/";
     private static Retrofit retrofit = null;
-
 
     public static Retrofit getClient() {
         if (retrofit == null) {
